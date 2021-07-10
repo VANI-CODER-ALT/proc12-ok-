521f8aaa4c3698b8931a9771d8a3e4f88d90dcc8
VANI-CODER-ALT <85821237+VANI-CODER-ALT@users.noreply.github.com>
2021-07-10 03:19:29 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+function preload(){
+  //pre-load images
+  
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+  invisible
+}
+
+function draw() {
+  background(0);
+
+}
